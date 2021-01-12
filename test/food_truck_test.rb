5395c9ab543e876d04a8e09b81b3bbd7d0539453
Wil McCauley <wmccauley724@gmail.com>
2021-01-12 13:08:44 -0700

    Initial commit

---
+++ test/food_truck_test.rb
@@ -0,0 +1,16 @@
+require_relative 'test_helper'
+
+class FoodTruckTest < Minitest::Test
+
+  def setup
+    @f = FoodTruck.new("Sunfisher")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end
+
+  def test_it_exists_with_attributes
+    assert_instance_of FoodTruck, @f
+    assert_equal "Sunfisher", @f.name
+    assert_equal Hash.new, @f.inventory
+  end
+end
