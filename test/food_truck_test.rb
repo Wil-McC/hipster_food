309155db278300ff736750de0aca4e337e6cb288
Wil McCauley <wmccauley724@gmail.com>
2021-01-12 13:26:17 -0700

    Add tests for stocking inventory

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -17,4 +17,15 @@ class FoodTruckTest < Minitest::Test
   def test_it_check_stock
     assert_equal 0, @f.check_stock(@item1)
   end
+
+  def test_it_can_stock_inventory
+    assert_equal 0, @f.check_stock(@item1)
+    @f.stock(@item1, 30)
+    assert_equal 30, @f.check_stock(@item1)
+    @f.stock(@item1, 25)
+    assert_equal 55, @f.check_stock(@item1)
+    @f.stock(@item2, 12)
+    assert_equal 55, @f.check_stock(@item1)
+    assert_equal 12, @f.check_stock(@item2)
+  end
 end
